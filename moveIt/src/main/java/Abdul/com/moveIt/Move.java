551e62de3ed04d8360adfdcd05a9804b11222185
abdul <abdulqaderutm@gmail.com>
2019-03-29 11:12:16 +0800

    fixing bugs

--- moveIt/src/main/java/Abdul/com/moveIt/Move.java
+++ moveIt/src/main/java/Abdul/com/moveIt/Move.java
@@ -3,6 +3,8 @@ package Abdul.com.moveIt;
 import android.app.Activity;
 import android.content.Context;
 
+import Abdul.com.movieit2.R;
+
 public class Move {
 
     public static void leftToRight(Context context){
